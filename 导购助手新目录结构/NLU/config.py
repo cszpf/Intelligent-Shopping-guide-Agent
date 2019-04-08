78bf33615649b344509cc00225a4fae68f6fa290
JansonKong <995710545@qq.com>
2019-04-08 20:55:33 +0800

    新的NLU模块第一次提交

---
+++ 导购助手新目录结构/NLU/config.py
@@ -0,0 +1,9 @@
+
+class Config(object):
+
+    """
+    Config class: the absolute path of the SLU package
+    """
+
+    def __init__(self):
+        self.SLU_path = 'D:/pythonWorkPrpject/NLU_TEST/SLU'  # fixme: please modify this path when in new environment
